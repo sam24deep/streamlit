9b1e262f24f7a4a72864bffbbade59d68283e510
Samandeep Singh <70514195+sam24deep@users.noreply.github.com>
2023-04-10 20:05:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,26 @@ from textblob import TextBlob
 import pandas as pd
 import streamlit as st
 import cleantext
+import base64
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-st.header('Sentiment Analysis')
+def add_bg_from_url():
+    st.markdown(
+        f"""
+         <style>
+         .stApp {{
+             background-image: url("https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2074&q=80");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+        unsafe_allow_html=True
+    )
+add_bg_from_url()
+st.header('Flysafe Sentiment Analysis')
 with st.expander('Analyze Text'):
     text = st.text_input("Text here:")
     if text:
@@ -18,18 +36,14 @@ with st.expander('Analyze CSV'):
     upl = st.file_uploader('Upload file')
 
 
-
-
     def score(x):
         blob1 = TextBlob(x)
         return blob1.sentiment.polarity
-
-
     #
     def analyze(x):
-        if x >= 0.5:
+        if x >= 0.1:
             return 'Positive'
-        elif x <= 0.3:
+        elif x <= 0:
             return 'Negative'
         else:
             return 'Neutral'
@@ -40,6 +54,7 @@ with st.expander('Analyze CSV'):
         df = pd.read_csv(upl)
         df['score'] = df['comment'].apply(score)
         df['analysis'] = df['score'].apply(analyze)
+        df['subjectivity'] = df['comment'].apply(lambda x: TextBlob(x).sentiment.subjectivity)
         st.write(df.head(10))
 
 
@@ -54,5 +69,66 @@ with st.expander('Analyze CSV'):
             label="Download data as CSV",
             data=csv,
             file_name='sentiment.csv',
-            mime='text/csv',
-        )
+            mime='text/csv',)
+        df['polarity'] = df['comment'].apply(lambda x: TextBlob(x).sentiment.polarity)
+        df['subjectivity'] = df['comment'].apply(lambda x: TextBlob(x).sentiment.subjectivity)
+        # Show Statisitcs
+        st.write('**Statistics:**')
+        st.write(df.describe())
+        # Show pie chart
+        st.write('**Pie Chart of Analysis Report:**')
+        fig, ax = plt.subplots()
+        sentiment_counts = df['analysis'].value_counts()
+        ax.pie(sentiment_counts, labels=sentiment_counts.index, autopct='%1.1f%%')
+        ax.axis('equal')
+        st.pyplot(fig)
+        # Show Distribution
+        st.write('**Distribution:**')
+        column = st.selectbox('Select a column', ['polarity', 'subjectivity'])
+        fig, ax = plt.subplots()
+        sns.histplot(df[column], kde=True, ax=ax)
+        st.pyplot(fig)
+        st.write('**Distribution:**')
+        column = st.selectbox('Select a column', ['score','analysis'])
+        fig, ax = plt.subplots()
+        sns.histplot(df[column], kde=True, ax=ax)
+        st.pyplot(fig)
+        # Show scatter plot
+        st.write('**Scatter Plot:**')
+        x = st.selectbox('Select X-axis', ['analysis'])
+        y = st.selectbox('Select Y-axis', ['polarity', 'subjectivity'])
+        fig, ax = plt.subplots()
+        sns.scatterplot(data=df, x=x, y=y, ax=ax)
+        st.pyplot(fig)
+        # Show Correlation
+        st.write('**Correlation:**')
+        fig, ax = plt.subplots()
+        sns.heatmap(df.corr(), annot=True, cmap='coolwarm', ax=ax)
+        st.pyplot(fig)
+        # Show histogram of sentiment polarity
+        st.write('**Histogram of Sentiment Polarity:**')
+        fig, ax = plt.subplots()
+        sns.histplot(df['analysis'], kde=True, ax=ax)
+        st.pyplot(fig)
+        # Generate word cloud
+        st.write('**Word Cloud:**')
+        fig, ax = plt.subplots()
+        wordcloud = WordCloud(width=800, height=800, background_color='white').generate(' '.join(df['comment']))
+        ax.imshow(wordcloud)
+        ax.axis('off')
+        st.pyplot(fig)
+        # Generate negative word cloud
+        st.write('**Negative Word Cloud:**')
+        fig, ax = plt.subplots()
+        wordcloud = WordCloud(width=800, height=800, background_color='white', colormap='Reds', max_words=50,
+                              ).generate(
+            ' '.join(df[df['polarity'] < 0]['comment']))
+        ax.imshow(wordcloud)
+        ax.axis('off')
+        st.pyplot(fig)
+
+
+
+
+
+
